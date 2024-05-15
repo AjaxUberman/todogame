6e0f2fedc2e7947a8acc1c01f66fda5806990b5d
AjaxUberman <bariskayikcidev@gmail.com>
2024-05-15 19:06:22 +0300

    quick fix

--- src/components/BuySection/BuyContent.js
+++ src/components/BuySection/BuyContent.js
@@ -4,7 +4,7 @@ import ContentCards from "./ContentCards";
 const BuyContent = () => {
   return (
     <div className="bg-buy-content-bg h-screen font-poetsen flex flex-col items-center">
-      <div className="py-10">
+      <div className="py-6">
         <h1 className="text-3xl tracking-wider border-b border-red-400 pb-2 text-red-400">
           Buy Items
         </h1>
